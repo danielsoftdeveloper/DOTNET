85ee8ea3181b5ea4ced133622bdfb79bcb16263c
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-07-22 15:39:18 -0300

    Finalized and tested menu.

--- DIO.Series/Program.cs
+++ DIO.Series/Program.cs
@@ -6,15 +6,52 @@ namespace DIO.Series
     {
         static void Main(string[] args)
         {
-           //Console.WriteLine("Hello World!");
+             string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						//ListarSeries();
+                        Console.WriteLine(" entrou no case 1");
+						break;
+					case "2":
+						//InserirSerie();
+                         Console.WriteLine(" entrou no case 2");
+						break;
+					case "3":
+						//AtualizarSerie();
+                         Console.WriteLine(" entrou no case 3");
+						break;
+					case "4":
+						//ExcluirSerie();
+                         Console.WriteLine(" entrou no case 4");
+						break;
+					case "5":
+						//VisualizarSerie();
+                         Console.WriteLine(" entrou no case 5");
+						break;
+					case "C":
+						Console.Clear();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+
+			Console.WriteLine("Obrigado atualizar nossos cadastro de serviços.");
+			Console.ReadLine();
         }
 
         private static string ObterOpcaoUsuario()
         {
             Console.WriteLine();
-            Console.WriteLine("DIO Séries a seu dispor!!!");
+            Console.WriteLine("CADASTRAR SÉRIES");
             Console.WriteLine("Informe a opção desejada:");
-
             Console.WriteLine("1- Listar séries");
             Console.WriteLine("2- Inserir nova série");
             Console.WriteLine("3- Atualizar série");
